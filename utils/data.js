25bc5f8bfcba18807fd7ff3a284e0263cf38978f
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-06 16:52:48 -0700

    added data and finished seed code, but not fully seeding db yet

--- utils/data.js
+++ utils/data.js
@@ -1,30 +1,53 @@
 const users = [
-  { id: 1, username: 'John', email: 'rando@email.com' },
-  { id: 2, username: 'Jane', email: 'lern@email.com' },
-  { id: 3, username: 'Mario', email: 'user@email.com' },
-  { id: 4, username: 'Kate', email: 'teach@email.com' },
-  { id: 5, username: 'Alex', email: 'lerner@email.com' },
+  {
+    id: 1,
+    username: "John",
+    email: "firstuzr@email.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    id: 2,
+    username: "Jane",
+    email: "teach@email.com",
+    thoughts: [],
+    friends: [],
+  },
+  { id: 3, username: "Jim", email: "stu@email.com", thoughts: [], friends: [] },
+  {
+    id: 4,
+    username: "Jack",
+    email: "lernr@email.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    id: 5,
+    username: "Jill",
+    email: "rando@email.com",
+    thoughts: [],
+    friends: [],
+  },
 ];
 
 const thoughts = [
-  { id: 1, thoughtText: 'Hello World', userId: 1 },
-  { id: 2, thoughtText: 'Lorem ipsum', userId: 2 },
-  { id: 3, thoughtText: 'I wonder if this works', userId: 3 },
-  { id: 4, thoughtText: 'I am a teacher', userId: 4 },
-  { id: 5, thoughtText: 'I am a student', userId: 5 },
-  { id: 6, thoughtText: 'I am a student', userId: 5 },
-  { id: 7, thoughtText: 'I am a student', userId: 5 },
+  { thoughtText: "Hello World", userId: 1, reactions: [] },
+  { thoughtText: "Lorem ipsum", userId: 2, reactions: [] },
+  { thoughtText: "I wonder if this works", userId: 3, reactions: [] },
+  { thoughtText: "I am a teacher", userId: 4, reactions: [] },
+  { thoughtText: "I am a student", userId: 5, reactions: [] },
 ];
 
 const getUser = (id) => {
   return users.find((user) => user.id === id);
-}; 
+};
 
 const getThoughts = (userId) => {
   return thoughts.filter((thought) => thought.userId === userId);
+
 };
 
-
+console.log(users, thoughts);
 module.exports = {
   users,
   getUser,
--- utils/seed.js
+++ utils/seed.js
@@ -1,65 +1,32 @@
-const connection = require('../config/connection');
-const {User, Thought} = require('../models');
-const {getUser, getThoughts} = require('../utils/data');
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { users, getThoughts } = require("../utils/data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('Connected to database');
+connection.once("open", async () => {
+  console.log("Connected to database");
 
   await User.deleteMany({});
   await Thought.deleteMany({});
-  console.log('Deleted all users and thoughts');
 
-  // create empty arrays to hold all users and thoughts
-
-  const users = [];
-  const thoughts = [];
-
-// after deleting all users and thoughts, create new users and thoughts by holding them in the arrays above
-
-  for (let i = 0; i < 5; i++) {
-    const user = await User.create(getUser(i + 1));
-    users.push(user);
+  if (users.length > 0) {
+    console.log("Seeding users");
+    for (const user of users) {
+      const currentUser = await User.create(user);
+      console.log(`Created user ${currentUser.username}`);
+      const thoughts = getThoughts(currentUser.id);
+      for (const thought of thoughts) {
+        await Thought.create(thought);
+        console.log(`Created thought ${thought.thoughtText}`);
+      }
+    }
+    console.log('Done seeding users and thoughts');
+  } else {
+    console.log("No users to seed");
   }
 
-  for (let i = 0; i < 3; i++) {
-    const thought = await Thought.create(getThoughts(i + 1));
-    thoughts.push(thought);
-  }
-
-  console.log('Created all users and thoughts');
-
-  // update the users with thoughts
-
-  for (let i = 0; i < 3; i++) {
-    const user = users[i];
-    const thought = thoughts[i];
-    user.thoughts.push(thought);
-    await user.save();
-  } 
-
-  console.log('Updated all users with thoughts');
-
-  // update the thoughts with reactions
-
-  for (let i = 0; i < 3; i++) {
-    const thought = thoughts[i];
-    const user = users[i];
-    thought.reactions.push({
-      reactionBody: 'This is a reaction',
-      username: user.username,
-    });
-    await thought.save();
-  } 
-
-  await User.collection.insertMany(users);
-  await Thought.collection.insertMany(thoughts);
-
-  console.log(users);
-  console.log(thoughts);
-  console.log('All done!');
+  console.log("Done seeding database s");
 
   process.exit(0);
 });
-
