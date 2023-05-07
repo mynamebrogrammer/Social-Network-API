f209a231219dde1f280295a3aa40f4883ad0d769
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-06 18:01:21 -0700

    single user, thought, all thoughts, and all users work flawlessly

--- utils/data.js
+++ utils/data.js
@@ -28,14 +28,86 @@ const users = [
     thoughts: [],
     friends: [],
   },
+  {
+    id: 6,
+    username: "Jenny",
+    email: "cor@email.com",
+    thoughts: [],
+    friends: [],
+  }
 ];
 
 const thoughts = [
-  { thoughtText: "Hello World", userId: 1, reactions: [] },
-  { thoughtText: "Lorem ipsum", userId: 2, reactions: [] },
-  { thoughtText: "I wonder if this works", userId: 3, reactions: [] },
-  { thoughtText: "I am a teacher", userId: 4, reactions: [] },
-  { thoughtText: "I am a student", userId: 5, reactions: [] },
+  {
+    username: 'John' ,
+    thoughtText: "Hello World", 
+    userId: 1, 
+    reactions: [
+      {
+        reactionBody: "Nice dude!",
+        username: "Jane",
+      },
+    ] 
+  },
+  {
+    username: 'Jane' ,
+    thoughtText: "Hello World",
+    userId: 2,
+    reactions: [
+      {
+        reactionBody: "Hellloooo!",
+        username: "John",
+      },
+    ]
+  },
+  {
+    username: 'Jim' ,
+    thoughtText: "I wonder what my friends are up to?",
+    userId: 3,
+    reactions: [
+      {
+        reactionBody: "Nothing really!",
+        username: "John",
+      },
+    ]
+  },
+  {
+    username: 'Jack' ,
+    thoughtText: "I'm hungry.",
+    userId: 4,
+    reactions: [
+      {
+        reactionBody: "Me too!",
+        username: "John",
+      },
+      {
+        reactionBody: "Me three!",
+        username: "Jane",
+      },
+    ]
+  },
+  {
+    username: 'Jill' ,
+    thoughtText: "I'm sleepy.",
+    userId: 5,
+    reactions: [
+      {
+        reactionBody: "Take a nap!",
+        username: "John",
+      },
+    ]
+  },
+  {
+    username: 'Jenny' ,
+    thoughtText: "I'm bored.",
+    userId: 6,
+    reactions: [
+      {
+        reactionBody: "Go for a walk!",
+        username: "John",
+      },
+    ]
+  },
 ];
 
 const getUser = (id) => {
@@ -44,10 +116,8 @@ const getUser = (id) => {
 
 const getThoughts = (userId) => {
   return thoughts.filter((thought) => thought.userId === userId);
-
 };
 
-console.log(users, thoughts);
 module.exports = {
   users,
   getUser,
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
-const { users, getThoughts } = require("../utils/data");
+const { users, thoughts } = require("../utils/data");
 
 connection.on("error", (err) => err);
 
@@ -10,23 +10,33 @@ connection.once("open", async () => {
   await User.deleteMany({});
   await Thought.deleteMany({});
 
-  if (users.length > 0) {
-    console.log("Seeding users");
-    for (const user of users) {
-      const currentUser = await User.create(user);
-      console.log(`Created user ${currentUser.username}`);
-      const thoughts = getThoughts(currentUser.id);
-      for (const thought of thoughts) {
-        await Thought.create(thought);
-        console.log(`Created thought ${thought.thoughtText}`);
-      }
-    }
-    console.log('Done seeding users and thoughts');
-  } else {
-    console.log("No users to seed");
+  // Delete all reactions from each thought
+  for (const thought of thoughts) {
+    thought.reactions = [];
   }
 
-  console.log("Done seeding database s");
+  // Create users and thoughts
+  for (const user of users) {
+    const createdUser = await User.create(user);
+    const userThoughts = thoughts.filter((thought) => thought.userId === user.id);
+    for (const thought of userThoughts) {
+      thought.username = createdUser.username;
+      thought.userId = createdUser.id;
+    }
+    const createdThoughts = await Thought.insertMany(userThoughts);
+    createdUser.thoughts = createdThoughts.map((thought) => thought.id);
+    await createdUser.save();
+  }
 
+  // Update friends for each user
+  for (const user of users) {
+    const createdUser = await User.findOne({ username: user.username });
+    const userFriends = users.find((u) => u.id === user.id).friends;
+    const friends = await User.find({ username: { $in: userFriends } });
+    createdUser.friends = friends.map((friend) => friend.id);
+    await createdUser.save();
+  }
+
+  console.log("Done seeding database");
   process.exit(0);
 });
