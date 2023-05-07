da12abcd5a5622e7b73da8dc14770502b5179c6a
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-07 01:54:48 -0700

    removed UserData from post thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,73 +1,68 @@
-const {Thought, User} = require('../models');
+const { Thought, User } = require("../models");
 
 const thoughtController = {
-    async getThoughts(req,res){
-        try {
-            const ThoughtData = await Thought.find()
-            .sort({createdAt: -1})
-            res.json(ThoughtData)
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    },
-    async getOneThought(req, res){
-        try {
-            const ThoughtData = await Thought.findOne({_id:req.params.thoughtId})
-            if (!ThoughtData) {
-                return res.status(404).json({message: 'No thought with this id'});
-            }
-            res.json(ThoughtData);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    },
-    async createThought(req, res){
-        try {
-            const ThoughtData = await Thought.create(req.body);
-            const UserData = await User.findOneAndUpdate(
-                {_id: req.body.userId},
-                {$push: {thoughts: ThoughtData._id}},
-                {new: true}
-            );
-            if (!UserData) {
-                return res.status(404).json({message: 'No user with this id'});
-            }
-            res.json(ThoughtData);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    },
-    async updateThought(req, res){
-        try {
-            const ThoughtData = await Thought.findOneAndUpdate(
-                {_id: req.params.thoughtId},
-                {$set: req.body},
-                {new: true}
-            );
-            if (!ThoughtData) {
-                return res.status(404).json({message: 'No thought with this id'});
-            }
-            res.json(ThoughtData);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    },
-    async deleteThought(req, res){
-        try {
-            const ThoughtData = await Thought.findOneAndDelete({_id: req.params.thoughtId});
-            if (!ThoughtData) {
-                return res.status(404).json({message: 'No thought with this id'});
-            }
-            res.json(ThoughtData);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
+  async getThoughts(req, res) {
+    try {
+      const ThoughtData = await Thought.find().sort({ createdAt: -1 });
+      res.json(ThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-}
+  },
+  async getOneThought(req, res) {
+    try {
+      const ThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+      if (!ThoughtData) {
+        return res.status(404).json({ message: "No thought with this id" });
+      }
+      res.json(ThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const ThoughtData = await Thought.create(req.body);
+
+      console.log(`Thought created for ${UserData.username}`);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async updateThought(req, res) {
+    try {
+      const ThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!ThoughtData) {
+        return res.status(404).json({ message: "No thought with this id" });
+      }
+      res.json(ThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteThought(req, res) {
+    try {
+      const ThoughtData = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+      if (!ThoughtData) {
+        return res.status(404).json({ message: "No thought with this id" });
+      }
+      res.json(ThoughtData);
+      console.log("Thought deleted");
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
 
 module.exports = thoughtController;
